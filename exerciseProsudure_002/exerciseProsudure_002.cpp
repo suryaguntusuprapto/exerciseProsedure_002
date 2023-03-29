d3d981cb8dd9803b81f2ab11753eebe594595571
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-03-29 14:21:50 +0700

    exerciseProsedure_002

--- exerciseProsudure_002/exerciseProsudure_002.cpp
+++ exerciseProsudure_002/exerciseProsudure_002.cpp
@@ -4,3 +4,12 @@ using namespace std;
 double rerata(double a, double b) {
     return (a + b) / 2;
 }
+
+string status(double a, double b) {
+    if (a >= 70 || b >= 80) {
+        return "diterima";
+    }
+    else {
+        return "ditolak";
+    }
+}
